5ce75e725599d2d59c0d763ce38c157c0a389bc4
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-05-13 13:07:03 +0500

    Uncomment Firebase configuration section

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-            // 1. Firebase Sozlamalari
+// 1. Firebase Sozlamalari
 const firebaseConfig = {
   apiKey: "AIzaSyBFOoT_ZhvE1tT1Qglh5GjPPhs8ZsyRWoc",
   authDomain: "energo-monitoring.firebaseapp.com",
