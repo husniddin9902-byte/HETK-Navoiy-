7631898ebdd487585227eaae302edafe8b08303c
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-05-30 10:03:34 +0500

    Update app.js

--- app.js
+++ app.js
@@ -23,6 +23,12 @@ let currentFolders = {};
 let activeFolderId = 'root'; 
 let editingFolderId = null;
 let activeMapMarkers = []; // Xaritadagi dinamik markerlarni nazorat qilish uchun massiv
+let currentUploadedImages = [];
+let telegramMessageIds = [];
+let editingElementId = null;
+
+let selectedFiles = [];
+let uploadedTelegramImages = [];
 
 // Google Satellit qatlami
 L.tileLayer('http://{s}.google.com/vt/lyrs=s,h&x={x}&y={y}&z={z}',{
