5538ba1a354e316acf541e5aa9e3fabb30ef015b
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-06-06 08:15:37 +0500

    Update app.js

--- app.js
+++ app.js
@@ -656,6 +656,7 @@ function refreshTreeDropdownSelection(container, selectedId) {
 // 1. Telegram Bot Sozlamalari (Orqa fonda 0 xarajat va bepul limit bilan rasmlarni saqlash uchun)
 const TELEGRAM_BOT_TOKEN = "8992286638:AAFPqW8OuFnBe-u6WZqqxiL1h3nhlIz48Qg"; // Bot tokeningizni shu yerga yozasiz
 const TELEGRAM_CHAT_ID = "-1003934340914"; // Maxfiy kanal yoki guruh IDsini yozasiz
+const TELEGRAM_ARCHIVE_CHAT_ID = "-1003885366930";
 
 async function deleteTelegramMessages(messageIds){
 if(!messageIds || !messageIds.length){
@@ -1216,7 +1217,7 @@ headers:{
 "Content-Type":"application/json"
 },
 body:JSON.stringify({
-chat_id: TELEGRAM_CHAT_ID,
+chat_id: TELEGRAM_ARCHIVE_CHAT_ID,
 media: media
 })
 }
