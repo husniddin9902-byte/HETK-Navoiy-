fb9ebd033e848fe4ab7e3635e3a8b258c08154a9
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-06-25 23:13:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1519,10 +1519,6 @@ needArchiveRebuild = true;
 database.ref('TPs').push();
 const tpId = newRef.key;
        
-elementData.tpId = tpId;
-         
-         elementData.tpPageLink =
-`https://husniddin9902-byte.github.io/HETK-Navoiy-/tp.html?id=${tpId}`;
          
 newRef.set(elementData).then(() => {
         showSaveLoader(100,"Yakunlanmoqda...");
