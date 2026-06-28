cee400d4889b0cfea168ffe89092246a8f2ec501
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-06-28 11:56:51 +0300

    Update input fields with coordinates on event
    Add coordinates to input fields if element panel is visible.

--- app.js
+++ app.js
@@ -134,6 +134,12 @@ map.on('contextmenu', function(e) {
         className: 'custom-popup'
     }).openPopup();
 
+// Agar element oynasi ochiq bo'lsa koordinatani unga ham yozamiz
+if(!elementManagePanel.classList.contains("hidden")){
+    inputLatitude.value = e.latlng.lat.toFixed(6);
+    inputLongitude.value = e.latlng.lng.toFixed(6);
+}
+  
     updatePanelValues(e.latlng.lat, e.latlng.lng, null, true);
     updateAddress(e.latlng.lat, e.latlng.lng, true);
 });
