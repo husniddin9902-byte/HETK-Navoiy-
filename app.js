056cca6e877c136d2544fc9e75f3e4aacca2e4ea
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-07-04 18:11:43 +0300

    Add alert to loadFilteredPoints function

--- app.js
+++ app.js
@@ -2191,6 +2191,7 @@ function isPointInsideFolder(pointFolderId, selectedFolderId){
 
 // 3. SIZ AYTGAN ASOSIY SCADA MANTIQI: Xaritada filtrlash, Birlashish va Miltillovchi markerlar (Override)
 function loadFilteredPoints() {
+  alert("1");
     const tpListContainer = document.getElementById('tp-list');
     if (!tpListContainer) return;
     
