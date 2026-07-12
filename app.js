ee5aae0ce13af4f535ec14f62c8211dcc0b1fd98
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-07-12 11:57:26 +0300

    Remove commented alert for first database entry
    Removed commented-out code for alerting the first item from allPoints.

--- app.js
+++ app.js
@@ -2966,10 +2966,6 @@ if (panelTabItems) {
         // Bazadan faqat tanlangan guruh ma'lumotlarini filtrlash
         database.ref('TPs').once('value', (snapshot) => {
             const allPoints = snapshot.val() || {};
-
-// MANA SHUNI QO'YING
-    const firstKey = Object.keys(allPoints)[0];
-    alert(JSON.stringify(allPoints[firstKey], null, 2));
           
             const keys = Object.keys(allPoints);
             const filteredKeys = activeFolderId === 'root' ? keys : keys.filter(key => allPoints[key].folderId === activeFolderId);
