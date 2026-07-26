64599ea6bb93e9c3d26072c0c3b6ef64d347af5d
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-07-26 07:15:23 +0300

    Simplify folder ID handling in attachResultsToTree
    Refactor attachResultsToTree to simplify folder handling.

--- app.js
+++ app.js
@@ -2803,19 +2803,12 @@ function buildFolderTree(){
 
 function attachResultsToTree(tree, results){
     results.forEach(tp=>{
-        let targetFolderId = null;
-      
-        // Avvalo joriy qidiruv papkasini topamiz
-        if(tp.folders){
-            const folderIds = Object.keys(tp.folders);
-            targetFolderId = folderIds.find(id => tree[id]);
-            if(!targetFolderId && folderIds.length){
-                targetFolderId = folderIds[0];
+        const folderIds = Object.keys(tp.folders || {});
+        folderIds.forEach(folderId=>{
+            if(tree[folderId]){
+                tree[folderId].items.push(tp);
             }
-        }
-        if(targetFolderId && tree[targetFolderId]){
-            tree[targetFolderId].items.push(tp);
-        }
+        });
     });
 }
 
