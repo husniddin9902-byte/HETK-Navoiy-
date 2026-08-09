e0906641010fe0187444744f75a82af9d708ddab
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-08-09 05:37:53 +0300

    Add detail name and power sections in app.js

--- app.js
+++ app.js
@@ -4276,6 +4276,15 @@ margin-bottom:16px;
 
 </div>
 
+<h2
+id="detail-name"
+style="
+margin:0 0 14px 0;
+font-size:34px;
+">
+Element nomi
+</h2>
+
 <div id="detail-power"
 style="margin-bottom:12px;">
 ⚡ Quvvat
