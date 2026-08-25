144828feeaf5fa6e0b80f1e59ee21872939fd144
husniddin9902-byte <husniddin.99.02@gmail.com>
2026-08-25 21:23:50 +0300

    Remove profile modal functions and add log
    Removed profile modal functionality and added a log statement.

--- profile-container.js
+++ profile-container.js
@@ -1,18 +1,1 @@
-const profileModal = document.getElementById("profile-container");
-const closeProfileBtn = document.getElementById("close-profile");
-
-function openProfile() {
-    if (profileModal) {
-        profileModal.style.display = "flex";
-    }
-}
-
-function closeProfile() {
-    if (profileModal) {
-        profileModal.style.display = "none";
-    }
-}
-
-if (closeProfileBtn) {
-    closeProfileBtn.addEventListener("click", closeProfile);
-}
+console.log("Profile module loaded");
